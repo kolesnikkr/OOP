1e2fc30a0ff87900289a7b62f3da25bf3737af10
kolesnikkr <152421935+kolesnikkr@users.noreply.github.com>
2025-06-26 10:37:07 +0300

    Add files via upload

--- 4/src/Main.java
+++ 4/src/Main.java
@@ -1,3 +1,5 @@
+import transactions.BankTransaction;
+
 public class Main {
     public static void main(String[] args) {
         BankTransaction transaction1 = new BankTransaction("T1", 200);
@@ -5,10 +7,5 @@ public class Main {
 
         BankTransaction transaction2 = new BankTransaction("T2", 500);
         transaction2.processTransaction();
-
-        Logger logger1 = Logger.getInstance();
-        Logger logger2 = Logger.getInstance();
-
-        System.out.println("Logger є одинаком: " + (logger1 == logger2));
     }
 }
